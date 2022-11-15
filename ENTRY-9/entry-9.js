5fcbe5ab51bb93b94085e52e865591bb3b893b49
devisdeving <makkd805@newschool.edu>
2022-11-15 15:27:58 -0500

    entry-9: text didnt change, but animations working

--- ENTRY-9/entry-9.js
+++ ENTRY-9/entry-9.js
@@ -1,16 +1,22 @@
 let highlightClass = 'highlight'; 
 let textBlock = document.querySelector('button'); 
 let button = document.querySelector('.clicks');
+let bg = document.querySelector('section');
+let backchange = 'backchange';
 
 button.onclick = () => {
-	if (button.getAttribute("data-text-swap") == button.innerHTML) {
-	  button.innerHTML = button.getAttribute("data-text-original");
+	  textBlock.classList.toggle(highlightClass);
+    }; if (button.getAttribute("data-text-swap") == button.innerHTML) {
+	  button.innerHTML = button.getAttribute("data-text-swap");
 	} else {
 	  button.setAttribute("data-text-original", button.innerHTML);
-	  button.innerHTML = button.getAttribute("data-text-swap");
-	}
-  }, false;
+	  button.innerHTML = button.getAttribute("data-text-original");
+	} false;
 
 button.onclick = () => {
 	textBlock.classList.toggle(highlightClass);
+};
+
+bg.onclick = () => {
+	bg.classList.toggle(backchange);
 };
